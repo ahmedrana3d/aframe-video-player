d1d2799a06134cc92dd3cf2a078aea4cf385460b
Ahmed001110 <af164021@gmail.com>
2024-02-10 02:27:20 +0500

    adding play on click to the hotspots

--- scripts/hotspot-click.component.js
+++ scripts/hotspot-click.component.js
@@ -5,11 +5,14 @@ schema : {
 init : function () {
     const el = this.el;
     const panaVideo = document.getElementById("panaroma-video");
+    const playBtn = document.getElementById("play-btn");
  const data = this.data;
 
  el.addEventListener("click" , ()=>{
     panaVideo.setAttribute("src" , data.videoSrc);
+
     panaVideo.play();
+    playBtn.setAttribute("src" , "#pause");
     console.log("Video is playing", data.videoSrc);
  })
 
